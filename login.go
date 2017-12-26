e47d716d1720d959a7937fcf01d63245c4855194
Jia Sui <jsfaint@gmail.com>
2017-12-27 00:13:40 +0800

    Define structure for login.go

--- login.go
+++ login.go
@@ -5,6 +5,21 @@ import (
 	"github.com/imroc/req"
 )
 
+type userInfo struct {
+	Userid      string `json:"userid"`
+	Phone       string `json:"phone"`
+	AccountType string `json:"account_type"`
+	BindPwd     int    `json:"bind_pwd"`
+	NickName    string `json:"nickname"`
+}
+
+//map[iRet:0 sMsg:ok data:map[userid:<user id> phone:<phone> account_type:4 bind_pwd:1 nickname:<phone>]]
+type logResp struct {
+	Ret  int      `json:"iRet"`
+	Mesg string   `json:"sMesg"`
+	Data userInfo `json:"data"`
+}
+
 /*
 login
 */
@@ -34,18 +49,18 @@ func login(r *req.Req, phone, pwd, dev, imei, sign string) (sessionid, userid st
 		}
 	}
 
-	var v map[string]interface{}
+	var v logResp
 	if err := resp.ToJSON(&v); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	if !checkStatus(v) {
+	if v.Ret != 0 {
 		fmt.Println("login fail")
 		return
 	}
 
-	fmt.Println(v, "\n")
+	fmt.Println(v.Data, "\n")
 
 	return
 }
