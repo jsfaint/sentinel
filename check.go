fbe6ba85b888cb3e31994ed4c789251028a887bb
Jia Sui <jsfaint@gmail.com>
2017-12-27 14:07:03 +0800

    Refactor struct complete, remove checkStatus()

--- check.go
+++ check.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"math/big"
-)
-
 type respHead struct {
 	Ret int    `json:"iRet"`
 	Msg string `json:"sMesg"`
@@ -15,17 +11,4 @@ func (h *respHead) success() bool {
 	} else {
 		return true
 	}
-}
-
-/*
-checkStatus checks the iRet status
-*/
-func checkStatus(v map[string]interface{}) bool {
-	ret, ok := v["iRet"]
-	if !ok {
-		return false
-	}
-
-	num := big.NewFloat(ret.(float64))
-	return (num.Cmp(big.NewFloat(0)) == 0)
 }
