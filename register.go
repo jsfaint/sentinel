3013272ee805d1ffd4c9f32ec66603db40dbef83
Jia Sui <jsfaint@gmail.com>
2017-12-27 13:33:03 +0800

    Refactor register.go

--- register.go
+++ register.go
@@ -6,31 +6,44 @@ import (
 )
 
 /*
+{
+  "iRet":0,
+  "sMsg":"ok",
+  "register":1
+}
+*/
+
+type registResp struct {
+	respHead
+	Register int `json:"register"`
+}
+
+/*
 checkRegister check if the phone number is already registered
 */
-func checkRegister(r *req.Req, phone string) {
+func checkRegister(r *req.Req, phone, sign string) (err error) {
 	fmt.Println("Phone:", phone)
 	param := req.Param{
 		"phone": phone,
+		"sign":  sign,
 	}
 
 	resp, err := r.Post(apiCheckAccountURL, headers, param)
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
-	var v map[string]interface{}
+	var v registResp
 
 	if err := resp.ToJSON(&v); err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
-	if !checkStatus(v) {
-		fmt.Println("checkRegister fail")
-		return
+	if !v.success() {
+		return fmt.Errorf("checkRegister fail")
 	}
 
 	fmt.Println(v, "\n")
+
+	return nil
 }
