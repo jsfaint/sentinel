ce1eeeffad754770b7d4b53ff25d500eefcc86ab
Jia Sui <jsfaint@gmail.com>
2018-01-01 00:23:26 +0800

    Add wkb draw

---
+++ draw.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/imroc/req"
+)
+
+type drawResp struct {
+	respHead
+}
+
+func (user *userReq) withDraw() (err error) {
+	r := user.r
+
+	sign := getSign(false, map[string]string{
+		"gasType":    "2",
+		"drawWkb":    user.accountInfo.Balance,
+		"appversion": appVersion,
+	})
+
+	body := req.Param{
+		"gasType":    "2",
+		"drawWkb":    user.accountInfo.Balance,
+		"appversion": appVersion,
+		"sign":       sign,
+	}
+
+	resp, err := r.Post(apiWKBDrawURL, body, headers)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(resp.Dump())
+
+	return
+}
