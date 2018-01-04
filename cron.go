fa5d6ba7162e79dfa715e26360598791a7689b4d
Jia Sui <jsfaint@gmail.com>
2018-01-04 08:05:28 +0800

    Fix wrong format of crontable

--- cron.go
+++ cron.go
@@ -5,13 +5,24 @@ import (
 	"github.com/robfig/cron"
 )
 
+/*
+Field name   | Mandatory? | Allowed values  | Allowed special characters
+----------   | ---------- | --------------  | --------------------------
+Seconds      | Yes        | 0-59            | * / , -
+Minutes      | Yes        | 0-59            | * / , -
+Hours        | Yes        | 0-23            | * / , -
+Day of month | Yes        | 1-31            | * / , - ?
+Month        | Yes        | 1-12 or JAN-DEC | * / , -
+Day of week  | Yes        | 0-6 or SUN-SAT  | * / , - ?
+*/
+
 var c *cron.Cron
 
 func startCrontable(users []*userReq) (err error) {
 	c = cron.New()
 
 	//Refresh data and check status every 30s
-	if err = c.AddFunc("@every 30s", func() {
+	if err = c.AddFunc("30 * * * * *", func() {
 		refresh(users)
 		checkStatus(users)
 	}); err != nil {
@@ -19,18 +30,20 @@ func startCrontable(users []*userReq) (err error) {
 	}
 
 	//Show summary every morning 9:00
-	if err = c.AddFunc("00 09 * * *", func() {
+	if err = c.AddFunc("0 0 9 * * *", func() {
 		summary(users)
 	}); err != nil {
 		return err
 	}
 
 	//Draw at 9:10 of the setting day
-	if err = c.AddFunc(fmt.Sprintf("10 09 * * %d", cfg.DrawDay), func() {
+	if err = c.AddFunc(fmt.Sprintf("0 10 9 * * %d", cfg.DrawDay), func() {
 		withDraw(users)
 	}); err != nil {
 		return err
 	}
+
+	fmt.Println(c.Entries())
 
 	c.Start()
 
